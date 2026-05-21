a28d0bebb6231e3de52e45526079726f03282301
xDosva <oddynucleth@gmail.com>
2026-05-20 19:57:10 -0600

    Base del proyecto en desarrollo

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,38 @@
 import random
 
+ia = random.randint(1,3)
 
 class Personaje():
+    def __init__(self, nombre, avatar):
+        self.nombre = nombre
+        self.avatar = avatar
     
+    def accion(self):
+        return int(input("Elige tu opción: "))
+
+
+
+
+Esteban = Personaje("Esteban","🦾")
+print(f"Valor de la IA: {ia}")
+valor = Esteban.accion()
+
+if valor == 1:
+    if ia == 2:
+        print("Gana IA")
+    if ia == 3:
+        print(f"Gana Esteban")
+elif valor == 2:
+    if ia == 1:
+        print("Gana IA")
+    if ia == 3:
+        print(f"Gana Esteban")
+elif valor == 3:
+    if ia == 1:
+        print(f"Gana Esteban")
+    if ia == 2:
+        print("Gana IA")
+elif valor == ia:
+    print("Empate")
+else:
+    print("Error")
