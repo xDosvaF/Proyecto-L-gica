9965ad927144166487326ad59dca82d972fb7072
xDosva <oddynucleth@gmail.com>
2026-05-23 23:22:01 -0600

    Nuevo proyecto

--- Proyecto.py
+++ Proyecto.py
@@ -1,56 +1,68 @@
 import random
-
-ia = random.randint(1,3)
-
-class Bonus():
-    def __init__(self, tipo, nombre, emoji, valor):
-        self.tipo: str = tipo
-        self.nombre: str = nombre
-        self.emoji: str = emoji
-        self.valor: int = valor
-
-Dinamita = Bonus("Activo","Dinamita","🧨",*5)
-Fuego = Bonus("Activo","Fuego","🔥",*2)
-Escudo = Bonus("Pasivo","Escudo","🛡️"*0,)
-Corazon = Bonus("Pasivo", "Corazon","❤️‍🩹",*0.5)
-Espada = Bonus("Activo","Espada","⚔️",+10)
-Curacion = Bonus("Pasivo","Curacion","💊",+10)
-
-
 class Personaje():
-    def __init__(self, nombre_personaje, avatar):
-        self.nombre_personaje = nombre_personaje
-        self.avatar = avatar
+    def __init__(self, _nombre_personaje, _avatar, _vida, _eleccion):
+        self.nombre_personaje = _nombre_personaje
+        self.avatar = _avatar
+        self.vida = _vida
+        self.eleccion = _eleccion
+    def mostrar_personaje(self):
+        return f"{self.nombre_personaje}:{self.avatar} | {self.eleccion} | {self.vida}"
+    def eleccion(self,opcion):
+        if opcion == 1:
+            self.eleccion = "✂️"
+        elif opcion == 2:
+            self.eleccion = "🗞️"
+        elif opcion == 3:
+            self.eleccion = "🪨"
+        else:
+            return "Opcion no válida"
     
-    def mostrar_presentacion(self):
-        return f"{self.nombre} | {self.avatar} "
-    
-    def accion(self):
-        return int(input("Elige tu opción: "))
+class InterfazJuego:
+    def mostrar_pantalla(self):
+        print("""
+              Opciones a Elegir:
+              1. ✂️
+              2. 🗞️
+              3. 🪨
+              """)
+    def mostrar_resultado(self,personaje,ia,victoria):
+        print("Los resultado son:")
+        print(personaje)
+        print(ia)
+        print(victoria)
+
+nombre = input("Escribe tu nombre: ")
+jugador = Personaje(nombre,"🫩",100,0)
+ia = Personaje("IA","🤖",100,0)
+interfaz = InterfazJuego()
+
+while True:
+
+    interfaz.mostrar_pantalla()
+    opcion_jugador = int(input("Elige una opcion: "))
+    opcion_ia = random.randint(1,3)
+
+    jugador.eleccion(opcion_jugador)
+    ia.eleccion(opcion_ia)
+
+    if opcion_jugador == 1:
+        if opcion_ia == 2:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado el jugador")
+        elif opcion_ia == 3:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado la IA")
+    elif opcion_jugador == 2:
+        if opcion_ia == 1:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado la IA")
+        elif opcion_ia == 3:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado el jugador")
+    elif opcion_jugador == 3:
+        if opcion_ia == 1:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado el jugador")
+        elif opcion_ia == 2:
+            interfaz.mostrar_resultado(jugador.mostrar_personaje(),ia.mostrar_personaje(), "Ha ganado la IA")
+    elif (opcion_jugador == opcion_ia):
+        print("EMPATE")
+    else:
+        print(jugador.eleccion())
 
 
-
-
-Esteban = Personaje("Esteban","🦾")
-print(f"Valor de la IA: {ia}")
-valor = Esteban.accion()
-
-if valor == 1:
-    if ia == 2:
-        print("Gana IA")
-    if ia == 3:
-        print(f"Gana {Esteban.mostrar_presentacion()}")
-elif valor == 2:
-    if ia == 1:
-        print("Gana IA")
-    if ia == 3:
-        print(f"Gana {Esteban.mostrar_presentacion()}")
-elif valor == 3:
-    if ia == 1:
-        print(f"Gana {Esteban.mostrar_presentacion()}")
-    if ia == 2:
-        print("Gana IA")
-elif valor == ia:
-    print("Empate")
-else:
-    print("Error")
