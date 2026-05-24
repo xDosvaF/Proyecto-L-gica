2f82989cd0803e26d1e6bbe1d47c3c0cef0f8836
xDosva <oddynucleth@gmail.com>
2026-05-23 23:52:34 -0600

    Agregue una barra de progreso, aunque aun le falta pulirla

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,5 @@
 import random
+import time
 class Personaje():
     def __init__(self, _nombre_personaje, _avatar, _vida, _eleccion):
         self.nombre_personaje = _nombre_personaje
@@ -7,7 +8,7 @@ class Personaje():
         self.eleccion = _eleccion
     def mostrar_personaje(self):
         return f"{self.nombre_personaje}:{self.avatar} | {self.eleccion} | {self.vida}"
-    def eleccion(self,opcion):
+    def eleccion_metodo(self,opcion):
         if opcion == 1:
             self.eleccion = "✂️"
         elif opcion == 2:
@@ -16,34 +17,49 @@ class Personaje():
             self.eleccion = "🪨"
         else:
             return "Opcion no válida"
-    
+
+class CadenaDeCarga:
+    def __init__(self, _cadena,_caracter,_b):
+        self.cadena = _cadena
+        self.caracter = _caracter
+        self.b = _b
+    def mostrar_progreso(self):
+        for i in range(100):
+            if(i % 2 == 0):
+                x = list(self.cadena)
+                x[self.b] = self.caracter
+                self.cadena = "".join(x)
+            
+            print(f"[{self.cadena}]{i+1}%",end='\r')
+            time.sleep(0.01)
+
 class InterfazJuego:
     def mostrar_pantalla(self):
-        print("""
-              Opciones a Elegir:
-              1. ✂️
-              2. 🗞️
-              3. 🪨
-              """)
+        print("Opciones a Elegir:")
+        print("1. ✂️")    
+        print("2. 🗞️")      
+        print("3. 🪨")
+
     def mostrar_resultado(self,personaje,ia,victoria):
         print("Los resultado son:")
         print(personaje)
         print(ia)
         print(victoria)
 
+barraProgreso = CadenaDeCarga("-"*50,"#",0)
 nombre = input("Escribe tu nombre: ")
 jugador = Personaje(nombre,"🫩",100,0)
 ia = Personaje("IA","🤖",100,0)
 interfaz = InterfazJuego()
 
 while True:
-
+    barraProgreso.mostrar_progreso()
     interfaz.mostrar_pantalla()
     opcion_jugador = int(input("Elige una opcion: "))
     opcion_ia = random.randint(1,3)
 
-    jugador.eleccion(opcion_jugador)
-    ia.eleccion(opcion_ia)
+    jugador.eleccion_metodo(opcion_jugador)
+    ia.eleccion_metodo(opcion_ia)
 
     if opcion_jugador == 1:
         if opcion_ia == 2:
